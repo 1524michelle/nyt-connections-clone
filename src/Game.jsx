c5d6ee952c7aa027673a8a8b4838ca3382cc8dbc
Michelle Huang <1524michelle@berkeley.edu>
2024-03-30 19:15:39 -0700

    fade from welcome page to game

--- src/Game.jsx
+++ src/Game.jsx
@@ -4,11 +4,11 @@ import { Grid, LoadingBar, Toolbar, Welcome } from './components';
 import './Game.css';
 
 function getDate() {
-    const today = new Date();
-    const month = today.toLocaleString('default', { month: 'long' });
-    const date = today.getDate();
-    const year = today.getFullYear();
-    return `${month} ${date}, ${year}`;
+  const today = new Date();
+  const month = today.toLocaleString('default', { month: 'long' });
+  const date = today.getDate();
+  const year = today.getFullYear();
+  return `${month} ${date}, ${year}`;
 }
 
 const Game = () => {
@@ -16,6 +16,7 @@ const Game = () => {
   const [date, setDate] = useState(getDate());
   const [loading, setLoading] = useState(true);
   const [showWelcome, setShowWelcome] = useState(true);
+  const [fadeIn, setFadeIn] = useState(true);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -25,30 +26,34 @@ const Game = () => {
   }, []);
 
   const handlePlay = () => {
-    setShowWelcome(false);
+    setFadeIn(false); // Trigger fade out animation
+    setTimeout(() => setShowWelcome(false), 300); // Hide Welcome component after animation
   };
 
   return (
-    <>
-      
-        <div className='app'>
-          <h1 id='game-header'>
-            <span className='game-name'>Connections</span>
-            <span id='date'>{date}</span>
-          </h1>
-          {loading ? (
-            <LoadingBar />
-          ) : showWelcome ? (
-            <Welcome onPlay={handlePlay} date={date} />
-          ) : (
-            <div>
+    <div className='app'>
+      <h1 id='game-header'>
+        <span className='game-name'>Connections</span>
+        <span id='date'>{date}</span>
+      </h1>
+      {loading ? (
+        <LoadingBar />
+      ) : (
+        <>
+          {showWelcome && (
+            <div className={`welcome-stage ${fadeIn ? 'fade-in' : 'fade-out'}`}>
+              <Welcome onPlay={handlePlay} date={date} />
+            </div>
+          )}
+          {!showWelcome && (
+            <div className='grid-stage'>
               <Toolbar />
               <Grid />
             </div>
-          )
-          }   
-        </div>
-    </>
+          )}
+        </>
+      )}
+    </div>
   );
 };
 
