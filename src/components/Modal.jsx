1b5c03bce6294a37a87e1bceaa9e059abf08fbd0
Michelle Huang <1524michelle@berkeley.edu>
2024-03-30 23:31:57 -0700

    modal slide out, no overlay fade yet

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,16 +5,21 @@ import closeIcon from '../assets/close.png';
 import './Modal.css';
 
 const Modal = ({ isOpen, onClose, children }) => {
-  const handleClose = () => {
-    onClose();
-  };
+  const [isClosing, setIsClosing] = useState(false);
 
+  const handleClose = () => {
+    setIsClosing(true);
+    setTimeout(() => {
+      onClose();
+      setIsClosing(false);
+    }, 250); // wait for the slideOut animation duration (0.25s) before closing
+  };
 
   return (
     <>
       {isOpen && (
-        <div className="modal-overlay" onClick={handleClose}>
-          <div className="modal" onClick={(e) => e.stopPropagation()}>
+        <div className={`modal-overlay ${isClosing ? 'closing' : ''}`} onClick={handleClose}>
+          <div className={`modal ${isClosing ? 'slideOut' : ''}`} onClick={(e) => e.stopPropagation()}>
             <img src={closeIcon} className="close-button" onClick={handleClose} />
             <div className="modal-content">{children}</div>
           </div>
@@ -25,9 +30,9 @@ const Modal = ({ isOpen, onClose, children }) => {
 };
 
 Modal.propTypes = {
-    isOpen: PropTypes.bool.isRequired,
-    onClose: PropTypes.func.isRequired,
-    children: PropTypes.node.isRequired
-}
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired
+};
 
 export default Modal;
