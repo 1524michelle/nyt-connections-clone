efd0cb8c5c5a0d70086ebe0299cb80a65710b1c4
Michelle Huang <1524michelle@berkeley.edu>
2024-04-13 15:04:37 -0700

    st up testing with RTL and jest, first test written for button component

---
+++ __mocks__/styleMock.js
@@ -0,0 +1,4 @@
+// __mocks__/styleMock.js
+
+// Mock CSS files for Jest
+export default {};
---
+++ src/components/Button.test.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { test, expect } from '@jest/globals';
+import '@testing-library/jest-dom';
+import Button from './Button';
+
+test('renders a button with the correct text', () => {
+  render(<Button text="Test" onClick={() => {console.log("test");}} isActive={true} />);
+  const buttonElement = screen.getByText(/test/i);
+  expect(buttonElement).toBeInTheDocument();
+});
