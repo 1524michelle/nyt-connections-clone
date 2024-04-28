e6ad6b15928562667102b0bc6a027ea251efd016
Michelle Huang <1524michelle@berkeley.edu>
2024-04-28 16:55:23 -0700

    add 404 page for urls that don't exist, doesn't account for the fact that the id could be anything though

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Game from './Game.jsx';
 import Create from './components/Create.jsx';
+import NotFoundPage from './components/NotFoundPage.jsx';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Game />} />
         <Route path="/:id" element={<Game />} />
         <Route path="/create" element={<Create />} />
+        <Route component={<NotFoundPage />} />
       </Routes>
     </Router>
   );
---
+++ client/src/components/NotFoundPage.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>404 - Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
