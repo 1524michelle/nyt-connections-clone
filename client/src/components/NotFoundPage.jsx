16333940e154a6df2f47352a1c5daf52b3fa5914
Michelle Huang <1524michelle@berkeley.edu>
2024-04-28 17:08:52 -0700

    not found page formatting

--- client/src/components/NotFoundPage.jsx
+++ client/src/components/NotFoundPage.jsx
@@ -1,9 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import './NotFoundPage.css';
 
 const NotFoundPage = () => {
   return (
-    <div>
+    <div className='not-found-page'>
       <h1>404 - Not Found</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
     </div>
